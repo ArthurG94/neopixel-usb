87ea5a7c454ecf72ff7f6b4e8327aefa1343142c
Arthur Guerry <arthur@guerry.me>
2026-05-02 09:40:33 +0200

    mise au propre du projet

--- software/src/main.c
+++ software/src/main.c
@@ -1,189 +1,365 @@
-﻿#include "stm32f0xx_hal.h"
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
 
-#define LED_NUMBER 1U
-#define WS2812_BITS_PER_LED 24U
-#define WS2812_RESET_SLOTS 100U
-#define WS2812_TIMER_PERIOD 60U
-#define WS2812_T0H_TICKS 20
-#define WS2812_T1H_TICKS 40
-#define WS2812_FRAME_SLOTS (LED_NUMBER * WS2812_BITS_PER_LED + WS2812_RESET_SLOTS)
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stm32f0xx_hal_tim.h"
+#include <string.h>
+/* USER CODE END Includes */
 
-static uint16_t ws2812Frame[WS2812_FRAME_SLOTS];
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
 
-static void Error_Handler(void)
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim17;
+DMA_HandleTypeDef hdma_tim17_ch1_up;
+
+PCD_HandleTypeDef hpcd_USB_FS;
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_TIM17_Init(void);
+static void MX_USB_PCD_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+
+
+// void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim){
+//   HAL_TIM_PWM_Stop_DMA(htim, TIM_CHANNEL_1); 
+// }
+
+uint16_t pwmData[24+100];
+
+void sendColor(uint8_t red, uint8_t green, uint8_t blue)
 {
-    while (1)
-    {
+
+  uint32_t color = ((uint32_t)green << 16) | ((uint32_t)red << 8) | blue; // GRB, MSB en premier
+
+  for (int i = 0; i < 24; i++)
+  {
+    if (color & (1 << (23 - i))){
+      pwmData[i] = 2/3.0 * LED_CNT; // bit 1
     }
+    else
+    {
+      pwmData[i] = 1/3.0 * LED_CNT; // bit 0
+    }
+  }
+
+  HAL_TIM_PWM_Start_DMA(&htim17, TIM_CHANNEL_1, (uint32_t*)pwmData, sizeof(pwmData) / sizeof(pwmData[0]));
+  
 }
 
-void SystemClock_Config(void)
-{
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+/* USER CODE END 0 */
 
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
-    RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-    {
-        Error_Handler();
-    }
-
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-    {
-        Error_Handler();
-    }
-
-    SystemCoreClockUpdate();
-
-    if (HAL_RCC_GetSysClockFreq() != 48000000U)
-    {
-        Error_Handler();
-    }
-}
-
-static void WS2812_GPIO_Init(void)
-{
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    GPIO_InitTypeDef gpio = {0};
-    gpio.Pin = GPIO_PIN_9;
-    gpio.Mode = GPIO_MODE_AF_PP;
-    gpio.Pull = GPIO_NOPULL;
-    gpio.Speed = GPIO_SPEED_FREQ_HIGH;
-    gpio.Alternate = GPIO_AF2_TIM17;
-    HAL_GPIO_Init(GPIOB, &gpio);
-}
-
-static void WS2812_TIM_DMA_Init(void)
-{
-    __HAL_RCC_TIM17_CLK_ENABLE();
-    __HAL_RCC_DMA1_CLK_ENABLE();
-
-    TIM17->CR1 = TIM_CR1_ARPE;
-    TIM17->PSC = 0U;
-    TIM17->ARR = WS2812_TIMER_PERIOD - 1U;
-    TIM17->CCR1 = 0U;
-    TIM17->CCMR1 = TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1M_1;
-    TIM17->CCER = TIM_CCER_CC1E;
-    TIM17->BDTR = TIM_BDTR_MOE;
-    TIM17->DIER = TIM_DIER_UDE;
-    TIM17->EGR = TIM_EGR_UG;
-
-    DMA1_Channel1->CCR = 0U;
-    DMA1_Channel1->CPAR = (uint32_t)&TIM17->CCR1;
-}
-
-static void WS2812_SetLedColor(uint8_t red, uint8_t green, uint8_t blue)
-{
-    uint32_t color;
-
-    uint32_t grb = ((uint32_t)green << 16) | ((uint32_t)red << 8) | (uint32_t)blue;
-
-    for (uint32_t bit = 0U; bit < WS2812_BITS_PER_LED; ++bit)
-    {
-        uint32_t mask = 1UL << (23U - bit);
-        ws2812Frame[bit] = (grb & mask) ? WS2812_T1H_TICKS : WS2812_T0H_TICKS;
-    }
-
-    for (uint32_t slot = WS2812_BITS_PER_LED; slot < WS2812_FRAME_SLOTS; ++slot)
-    {
-        ws2812Frame[slot] = 0U;
-    }
-}
-
-static HAL_StatusTypeDef WS2812_Send(void)
-{
-    DMA1_Channel1->CCR &= ~DMA_CCR_EN;
-    DMA1->IFCR = DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1;
-
-    TIM17->CR1 &= ~TIM_CR1_CEN;
-    TIM17->CNT = 0U;
-    TIM17->CCR1 = ws2812Frame[0];
-
-    DMA1_Channel1->CMAR = (uint32_t)&ws2812Frame[1];
-    DMA1_Channel1->CNDTR = WS2812_FRAME_SLOTS - 1U;
-    DMA1_Channel1->CCR = DMA_CCR_DIR | DMA_CCR_MINC | DMA_CCR_PSIZE_0 | DMA_CCR_MSIZE_0 | DMA_CCR_PL_1;
-
-    DMA1_Channel1->CCR |= DMA_CCR_EN;
-    TIM17->CR1 |= TIM_CR1_CEN;
-
-    uint32_t deadline = HAL_GetTick() + 10U;
-    while ((DMA1->ISR & (DMA_ISR_TCIF1 | DMA_ISR_TEIF1)) == 0U)
-    {
-        if (HAL_GetTick() > deadline)
-        {
-            DMA1_Channel1->CCR &= ~DMA_CCR_EN;
-            TIM17->CR1 &= ~TIM_CR1_CEN;
-            TIM17->CCR1 = 0U;
-            DMA1->IFCR = DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1;
-            return HAL_TIMEOUT;
-        }
-    }
-
-    uint32_t isr = DMA1->ISR;
-
-    DMA1_Channel1->CCR &= ~DMA_CCR_EN;
-    TIM17->CR1 &= ~TIM_CR1_CEN;
-    TIM17->CCR1 = 0U;
-    DMA1->IFCR = DMA_IFCR_CGIF1 | DMA_IFCR_CTCIF1 | DMA_IFCR_CHTIF1 | DMA_IFCR_CTEIF1;
-
-    if ((isr & DMA_ISR_TEIF1) != 0U)
-    {
-        return HAL_ERROR;
-    }
-
-    return HAL_OK;
-}
-
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
-    volatile HAL_StatusTypeDef wsStatus = HAL_OK;
 
-    HAL_Init();
-    SystemClock_Config();
+  /* USER CODE BEGIN 1 */
 
-    WS2812_GPIO_Init();
-    WS2812_TIM_DMA_Init();
+  /* USER CODE END 1 */
 
-    while (1)
-    {
-        WS2812_SetLedColor(255, 0U, 0U);
-        wsStatus = WS2812_Send();
+  /* MCU Configuration--------------------------------------------------------*/
 
-        if (wsStatus != HAL_OK)
-        {
-            Error_Handler();
-        }
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-        HAL_Delay(300U);
+  /* USER CODE BEGIN Init */
 
-        WS2812_SetLedColor(0U, 24U, 0U);
-        wsStatus = WS2812_Send();
+  /* USER CODE END Init */
 
-        if (wsStatus != HAL_OK)
-        {
-            Error_Handler();
-        }
+  /* Configure the system clock */
+  SystemClock_Config();
 
-        HAL_Delay(300U);
+  /* USER CODE BEGIN SysInit */
 
-        WS2812_SetLedColor(0U, 0U, 24U);
-        wsStatus = WS2812_Send();
+  /* USER CODE END SysInit */
 
-        if (wsStatus != HAL_OK)
-        {
-            Error_Handler();
-        }
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_TIM17_Init();
+  MX_USB_PCD_Init();
+  /* USER CODE BEGIN 2 */
 
-        HAL_Delay(300U);
+  memset(pwmData, 0, sizeof(pwmData));
+  sendColor(0,0,255);
+
+
+  
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    for(int red = 0; red < 256; red++){
+      sendColor(red, 0, 0);
+      HAL_Delay(2000/256);
     }
+
+
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
 }
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL12;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM17 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM17_Init(void)
+{
+
+  /* USER CODE BEGIN TIM17_Init 0 */
+
+  /* USER CODE END TIM17_Init 0 */
+
+  TIM_OC_InitTypeDef sConfigOC = {0};
+  TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+  /* USER CODE BEGIN TIM17_Init 1 */
+
+  /* USER CODE END TIM17_Init 1 */
+  htim17.Instance = TIM17;
+  htim17.Init.Prescaler = 0;
+  htim17.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim17.Init.Period = LED_CNT -1 ;
+  htim17.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim17.Init.RepetitionCounter = 0;
+  htim17.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim17) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim17) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+  if (HAL_TIM_PWM_ConfigChannel(&htim17, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+  sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+  sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+  sBreakDeadTimeConfig.DeadTime = 0;
+  sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+  sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+  sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+  if (HAL_TIMEx_ConfigBreakDeadTime(&htim17, &sBreakDeadTimeConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM17_Init 2 */
+
+  /* USER CODE END TIM17_Init 2 */
+  HAL_TIM_MspPostInit(&htim17);
+
+}
+
+/**
+  * @brief USB Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USB_PCD_Init(void)
+{
+
+  /* USER CODE BEGIN USB_Init 0 */
+
+  /* USER CODE END USB_Init 0 */
+
+  /* USER CODE BEGIN USB_Init 1 */
+
+  /* USER CODE END USB_Init 1 */
+  hpcd_USB_FS.Instance = USB;
+  hpcd_USB_FS.Init.dev_endpoints = 8;
+  hpcd_USB_FS.Init.speed = PCD_SPEED_FULL;
+  hpcd_USB_FS.Init.phy_itface = PCD_PHY_EMBEDDED;
+  hpcd_USB_FS.Init.low_power_enable = DISABLE;
+  hpcd_USB_FS.Init.lpm_enable = DISABLE;
+  hpcd_USB_FS.Init.battery_charging_enable = DISABLE;
+  if (HAL_PCD_Init(&hpcd_USB_FS) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USB_Init 2 */
+
+  /* USER CODE END USB_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
